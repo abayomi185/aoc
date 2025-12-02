47c509c82350ab1cc39d58f9e54ed3134688daf2
Yomi Ikuru <captyomjnr@gmail.com>
2025-12-02 03:26:20 +0000

    feat: add 2025 day 1 part 1

--- aoc2025/src/day_01/mod.rs
+++ aoc2025/src/day_01/mod.rs
@@ -1,6 +1,32 @@
 #[allow(dead_code)]
 fn part_1(input: &str) -> i32 {
-    1
+    let mut count = 0;
+
+    let mut rotation_index = 50;
+
+    for line in input.lines() {
+        let coordinates = line.split_at(1);
+        let direction = coordinates.0;
+        let distance: i32 = coordinates.1.parse().unwrap();
+
+        match direction {
+            "L" => {
+                // Move left (negative)
+                rotation_index -= distance;
+            }
+            "R" => {
+                // Move right (positive)
+                rotation_index += distance;
+            }
+            _ => {}
+        }
+
+        if rotation_index == 0 || (rotation_index % 100) == 0 {
+            count += 1;
+        }
+    }
+
+    count
 }
 
 #[allow(dead_code)]
@@ -15,29 +41,39 @@ mod tests {
     use super::*;
 
     const INPUT: &str = r#"
-    "#;
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"#;
 
     #[test]
     fn test_part_1() {
         let result = part_1(INPUT.trim_start_matches('\n'));
-        assert_eq!(result, 1);
+        assert_eq!(result, 3);
     }
 
     #[test]
     fn run_part_1() {
         let result = part_1(&read_input(file!()));
-        assert_eq!(result, 1)
+        assert_eq!(result, 1135)
     }
 
-    #[test]
-    fn test_part_2() {
-        let result = part_2(INPUT.trim_start_matches('\n'));
-        assert_eq!(result, 1);
-    }
-
-    #[test]
-    fn run_part_2() {
-        let result = part_2(&read_input(file!()));
-        assert_eq!(result, 1)
-    }
+    // #[test]
+    // fn test_part_2() {
+    //     let result = part_2(INPUT.trim_start_matches('\n'));
+    //     assert_eq!(result, 1);
+    // }
+    //
+    // #[test]
+    // fn run_part_2() {
+    //     let result = part_2(&read_input(file!()));
+    //     assert_eq!(result, 1)
+    // }
 }
